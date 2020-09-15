4c6d875b7db37e783d6cf4e556004b0950ecb85a
yize <3011429848@qq.com>
2020-09-15 19:54:33 +0800

    feat:implementation servlet

--- src/main/java/com/yize/douban/servlet/GetPhoto.java
+++ src/main/java/com/yize/douban/servlet/GetPhoto.java
@@ -3,12 +3,14 @@ package com.yize.douban.servlet;
 import com.yize.douban.module.CelebrityPhoto;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+@WebServlet("/api/photo")
 public class GetPhoto extends HttpServlet {
     private static CelebrityPhoto celebrityPhoto;
     @Override
